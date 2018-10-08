84fcd3a76a9a58a01de63d5ea6343cfb86bd4894
Jens Nevens <jnevens@vub.ac.be>
2018-10-08 10:45:18 +0200

    Smaller version of Babel2 release for AIPP students

--- applications/fcg-for-planning/package.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :common-lisp-user)
-
-(defpackage :planning
-  (:documentation "A package for bidirectional planning (planning + plan-recognition) in Fluid
-                   Construction Grammar.")
-  (:use :common-lisp :utils :monitors :web-interface :fcg))
-
--- grammars/Dutch/dutch-vp/package.lisp
+++
@@ -1,5 +0,0 @@
-
-(in-package :cl-user)
-
-(defpackage :dutch-vp
-  (:use :common-lisp :utils :monitors :web-interface :fcg :network))
