5ecf59331e18c0379ca85febf3ee50002a1b173a
Jens Nevens <jnevens@vub.ac.be>
2018-10-10 12:20:08 +0200

    Pointers to useful functions

--- experiments/basic-naming-game/world.lisp
+++ experiments/basic-naming-game/world.lisp
@@ -2,3 +2,6 @@
 
 ;;;; Use this file to define and create the world
 ;;;; in which the interactions take place.
+;;;; If you want to define a symbol denoting a
+;;;; unique object, you can use the utility
+;;;; function #'make-id, e.g. (make-id 'obj)
