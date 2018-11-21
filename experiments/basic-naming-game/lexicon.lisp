7bc41261b2e6b5aa5a5b82ac276f3c983b085720
Jens Nevens <jnevens@vub.ac.be>
2018-11-21 18:16:55 +0100

    Removed BNG skeleton

--- experiments/basic-naming-game/lexicon.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :basic-naming-game)
-
-;;;; Use this file to define the lexicon of the agent.
-;;;; This includes functions for adding things to the
-;;;; lexicon, finding things in the existing lexicon
-;;;; or updating scores.
-;;;; TIP: There is a utility function for creating word
-;;;; forms with random syllables: #'make-new-word
--- experiments/basic-naming-game/world.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :basic-naming-game)
-
-;;;; Use this file to define and create the world
-;;;; in which the interactions take place.
-;;;; If you want to define a symbol denoting a
-;;;; unique object, you can use the utility
-;;;; function #'make-id, e.g. (make-id 'obj)
